51e94ccccaf3ee64ebc431126a1f08877d66cb92
Diniz <127336377+Diniz69food@users.noreply.github.com>
2026-03-12 14:05:02 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,67 +1,47 @@
 #include <iostream>
-#include <string>
-
-using namespace std;
-
-// Classe que representa os lutadores do jogo
-class Personagem {
-public:
-    string nome;
-    int vida;
-    int ataque;
-    int pacoes;
-
-    Personagem(string n, int v, int a) {
-        nome = n;
-        vida = v;
-        ataque = a;
-        pacoes = 1; 
-    }
-
-    void atacar(Personagem &inimigo) {
-        cout << " -> " << nome << " atacou " << inimigo.nome << "!" << endl;
-        inimigo.vida -= ataque;
-        if (inimigo.vida < 0) inimigo.vida = 0;
-    }
-
-    void usarPocao() {
-        if (pacoes > 0) {
-            vida += 30;
-            pacoes--;
-            cout << " -> " << nome << " bebeu uma porcao! +30 de vida imediato." << endl;
-        } else {
-            cout << " -> Voces nao tem mais porcoes!" << endl;
-        }
-    }
-};
+#include "Personagem.h"
 
 int main() {
-    Personagem heroi("Arthur", 100, 20);
-    Personagem monstro("Goblin", 50, 15);
+    Personagem heroi("Arthur, o Cavaleiro", 120, 25, 50);
+    Personagem monstro("Dragao de PDS", 200, 15, 100);
 
-    cout << "=== MOTOR DE RPG SIMPLIFICADO ===" << endl;
+    // Limpa o arquivo de log antigo
+    ofstream log("batalha.txt");
+    log << "--- INICIO DA BATALHA ---\n";
+    log.close();
 
-    while (heroi.vida > 0 && monstro.vida > 0) {
-        int escolha;
-        cout << "\nStatus: " << heroi.nome << " [" << heroi.vida << " HP] | " 
-             << monstro.nome << " [" << monstro.vida << " HP]" << endl;
-        
-        cout << "Escolha: (1) Atacar (2) Usar Porcao: ";
-        cin >> escolha;
+    cout << "--- BEM-VINDO AO RPG DA UFMG ---" << endl;
 
-        if (escolha == 1) heroi.atacar(monstro);
-        else heroi.usarPocao();
+    while (heroi.estaVivo() && monstro.estaVivo()) {
+        heroi.mostrarStatus();
+        monstro.mostrarStatus();
 
-        if (monstro.vida <= 0) {
-            cout << "\n[VITORIA] O monstro foi derrotado!" << endl;
-            break; 
+        cout << "\nSUA VEZ: (1) Atacar | (2) Usar Pocao | (3) Fugir: ";
+        int acao;
+        cin >> acao;
+
+        if (acao == 1) {
+            heroi.atacar(monstro);
+        } else if (acao == 2) {
+            heroi.usarItem(0);
+        } else {
+            cout << "Voce tentou fugir, mas falhou!" << endl;
         }
 
-        // Vez do Monstro
+        if (!monstro.estaVivo()) {
+            cout << "\nO " << monstro.nome << " foi derrotado! Parabens!" << endl;
+            break;
+        }
+
+        // Turno do Inimigo
+        cout << "\n--- TURNO DO INIMIGO ---" << endl;
         monstro.atacar(heroi);
 
-        if (heroi.vida <= 0) cout << "\n[DERROTA] Voce caiu em combate!" << endl;
+        if (!heroi.estaVivo()) {
+            cout << "\nVoce caiu em batalha... O semestre acabou para voce." << endl;
+        }
     }
 
+    cout << "\n[DICA]: O resumo da luta foi salvo em 'batalha.txt'!" << endl;
     return 0;
 }
